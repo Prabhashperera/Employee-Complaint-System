c6e0df6ec8dac854965f4bb10626d22dac7b7e9a
Prabhashperera <prabashmperera01@gmail.com>
2025-06-12 00:18:31 +0530

    Sign IN & Sign UP UI

--- src/main/java/org/system/controller/ComplaintsServlet.java
+++ src/main/java/org/system/controller/ComplaintsServlet.java
@@ -7,12 +7,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.system.model.ComplaintModel;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/home")
 public class ComplaintsServlet extends HttpServlet {
@@ -24,6 +27,20 @@ public class ComplaintsServlet extends HttpServlet {
         try {
             PreparedStatement stm = ds.getConnection().prepareStatement("select * from complaints");
             ResultSet rs = stm.executeQuery();
+            List<ComplaintModel> complaints = new ArrayList<>();
+            while (rs.next()) {
+                ComplaintModel complaint = new ComplaintModel();
+                complaint.setId(rs.getString("id"));
+                complaint.setTitle(rs.getString("title"));
+                complaint.setDescription(rs.getString("description"));
+                complaint.setPriority(rs.getString("priority"));
+                complaint.setStatus(rs.getString("status"));
+                complaint.setSubmittedBy(req.getParameter("submitted_by"));
+                complaint.setSubmittedTime(rs.getString("submitted_at"));
+                complaints.add(complaint);
+            }
+
+            resp.getWriter().print(complaints);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
