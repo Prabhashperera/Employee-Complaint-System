9c18b518df46a600f6008025191497047a8b7327
Prabhashperera <prabashmperera01@gmail.com>
2025-06-11 23:47:24 +0530

    Database Config Complete

--- src/main/java/org/system/controller/ComplaintsServlet.java
+++ src/main/java/org/system/controller/ComplaintsServlet.java
@@ -1,18 +1,31 @@
 package org.system.controller;
 
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet("/home")
 public class ComplaintsServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/complaint_db")
+    private DataSource ds;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello World");
+        try {
+            PreparedStatement stm = ds.getConnection().prepareStatement("select * from complaints");
+            ResultSet rs = stm.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
